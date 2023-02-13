396558a747532154de1d9b4c662f53d9ffd0341a
david sureway <nwachukwudavid100@gmail.com>
2023-02-13 04:08:52 +0100

    -

---
+++ project2/README.md
@@ -0,0 +1,5 @@
+# numpy is a scientific computing package in python
+
+# pip install caer
+
+reading images and videos
---
+++ project2/read.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+# reading images in opencv--------------------------
+
+# readImg = cv.imread('assets/humanFace.png')
+# cv.imshow('Nice Face', readImg)
+# cv.waitKey(0)
+
+# reading videos in opencv-----------------------
+
+# 0 => your webCam
+# 1 => your first camera
+# but an absolute or relative path will be in quotes
+
+# capture = cv.VideoCapture(0)
+capture = cv.VideoCapture('assets/file_example.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('NEW video', frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+
+# print('this is done')
